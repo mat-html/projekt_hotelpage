cb699d53afa9e96754d4ffb24e2a046247bf1c9f
matic219 <matic.jovan219@gmail.com>
2024-11-11 20:08:52 +0100

    index.php statt Muster_Hotel.php

--- sign_in.php
+++ sign_in.php
@@ -21,22 +21,26 @@
     <div class="container">
         <form action="process-signup.php" method="POST">
             <div class="input-group mt-3">
+                <label for="last-name"></label>
                 <span class="input-group-text">Last name</span>
                 <input type="text" class="form-control" name="last-name" id="last-name" placeholder="Enter your last name" required>
             </div>
             <br>
             <div class="input-group">
+                <label for="first-name"></label>
                 <span class="input-group-text">First name</span>
                 <input type="text" class="form-control" name="first-name" id="first-name" placeholder="Enter your first name" required>
             </div>
             <br>
             <div class="input-group">
+                <label for="email"></label>
                 <span class="input-group-text">E-mail</span>
                 <input type="text" class="form-control" name="email" id="email" placeholder="name@example.com" required>
             </div>
             <br>
             <div class="input-group">
-            <span class="input-group-text">Password</span>
+                <label for="password"></label>
+                <span class="input-group-text">Password</span>
                 <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password" required>
             </div>
             <br>
@@ -47,24 +51,24 @@
             </div>
             <br>
             <div class="input-group">
+                <label for="phone-numb"></label>
                 <span class="input-group-text">Phone number</span>
                 <input type="text" class="form-control" name="phone-numb" id="phone-numb" placeholder="123 456 789" required>
             </div>
             <br>
             <div class="input-group">
+                <lable for="date-of-birth"></lable>
                 <span class="input-group-text">Date of birth</span>
                 <input class="form-control" type="date" name="date-of-birth" id="date-of-birth" placeholder="DD.MM.YYYY" required>
             </div>
             <br>
-            <div class="form-select">
-                <div class="input-group">
-                    <span class="input-group-text">Gender</span>
-                    <select for="gender" name="gender" class="form-control">
-                            <option name="gender" id="female" value="female">Female</option>
-                            <option name="gender" id="male" value="male">Male</option>
-                            <option name="gender" id="other" value="other">Other</option> 
-                    </select>
-            </div>
+            <div class="input-group">
+                <span class="input-group-text">Gender</span>
+                <select for="gender" name="gender" class="form-control">
+                    <option name="gender" id="female" value="female">Female</option>
+                    <option name="gender" id="male" value="male">Male</option>
+                    <option name="gender" id="other" value="other">Other</option> 
+                </select>
             </div>
             <br>
             <div class="d-grid">
