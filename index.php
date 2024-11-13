bd6cf6abc268dccbf93be2ba69db7b437e4ed6e8
matic219 <matic.jovan219@gmail.com>
2024-11-13 19:58:45 +0100

    css in bearbeitung

--- index.php
+++ index.php
@@ -1,22 +1,27 @@
 <?php
 
-  include("navbar.php");
+include("navbar.php");
 
 ?>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12 text-center ">
-                
-                <?php 
-                    if(isset($_SESSION["useruid"])){
-                        echo '<h1 class="mt-3">Willkommen back, '. $_SESSION["useruid"] .'</h1>';
-                    }
-                ?>
 
-            </div>
+<section id="hero" class="min-vh-100 d-flex align-items-center">
+        <div class=" container">
+    <div class="row">
+        <div class="col-sm-12 text-center">
+            <?php
+            if (isset($_SESSION["useruid"])) {
+                echo '<h1 class="display-1">Welcome back, ' . $_SESSION["useruid"] . '!</h1>';
+            } else {
+                echo '<h1 class="display-1">Welcome to Muster Hotel!</h1>';
+                echo '<a class="btn btn-brand mt-5" href="sign_up.php">Sign up</a></li>';
+                echo '<a class="btn btn-brand login mt-5" href="login.php">Log in</a></li>';
+            }
+            ?>
         </div>
     </div>
+</section>
+
 <?php
-    include("footer.php");
+include("footer.php");
 ?>
--- navbar.php
+++ navbar.php
@@ -1,28 +1,30 @@
 <?php
-  session_start();
+session_start();
 ?>
 
 
 <!DOCTYPE html>
-<html lang="de">
+<html lang="en">
+
 <head>
-    <title>Muster Hotel</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-</head> 
-<body>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
+  <title>Muster Hotel</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="my.css/style.css">
+  <body>
+  <header>
+    <nav class="navbar navbar-expand-md">
+      <div class="container">
         <a class="navbar-brand" href="index.php">Muster Hotel</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
+          <ul class="navbar-nav ms-auto ">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+              <a class="nav-link" aria-current="page" href="index.php">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="news.php">News</a>
@@ -34,17 +36,15 @@
               <a class="nav-link" href="hilfe.php">Hilfe</a>
             </li>
             <?php
-              if(isset($_SESSION["useruid"])){
-                echo '<li class="nav-item"><a class="nav-link" href="profile.php">Profile Page</a></li>';
-                echo '<li class="nav-item"><a class="nav-link" href="includes/logout.php">Log out</a></li>';
-              }
-              else {
-                echo '<li class="nav-item"><a class="nav-link" href="sign_up.php">Sign up</a></li>';
-                echo '<li class="nav-item"><a class="nav-link" href="login.php">Log in</a></li>';
-              }
+            if (isset($_SESSION["useruid"])) {
+              echo '<li class="nav-item"><a class="nav-link" href="profile.php">Profile Page</a></li>';
+              echo '<li class="nav-item"><a class="nav-link" href="includes/logout.php">Log out</a></li>';
+            }
             ?>
-            
+
           </ul>
         </div>
       </div>
     </nav>
+  </header>
+  <main class="flex-shrink-0">
